251775a0a7117287fb28e7eefd752b661dfad6f8
Sarah Ilse Daniels, PhD <107085908+SIDaniels@users.noreply.github.com>
2023-01-25 00:24:00 -0800

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -17,7 +17,7 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 # in callbacks to transform the output value to logarithmic
 
 # Connect to database
-engine = create_engine(os.environ['MYSQL_URL'])
+engine = create_engine(os.environ['MYSQLDATABASE'])
 
 
 app.layout = html.Div([
